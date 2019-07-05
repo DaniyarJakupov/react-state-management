4d16d5ebddc2cf3ef4a9a1ca4da61bc4d223e1e4
Daniyar Jakupov <ddr199305@gmail.com>
2019-07-05 21:42:58 +0600

    Implement simple state

--- simple-counter/src/index.js
+++ simple-counter/src/index.js
@@ -4,18 +4,28 @@ import { render } from 'react-dom';
 import './styles.scss';
 
 class Counter extends Component {
+  state = { counter: 0 };
+
   render() {
+    const { counter } = this.state;
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{counter}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={() => this.setState(this.props.onIncrement)}>
+            Increment
+          </button>
+          <button onClick={() => this.setState({ counter: counter - 1 })}>
+            Decrement
+          </button>
+          <button onClick={() => this.setState({ counter: 0 })}>Reset</button>
         </section>
       </main>
     );
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+render(
+  <Counter onIncrement={state => ({ counter: state.counter + 500 })} />,
+  document.getElementById('root'),
+);
